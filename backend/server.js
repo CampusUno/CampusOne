11e3ba0ed670f3aabdf99bd15b2c12c97328c864
Pratik Kuril <pratikkuril16@gmail.com>
2024-11-23 05:32:07 +0530

    Add basic routes and create a .env with mongo-uri

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from "express";
 
+import authRoutes from "./routes/auth.routes.js"; //Add the extension when importing local files
+//What is the authRoutes? authRoutes is an alias to the Router defined in auth.routes.js, meaning instead of calling it router we will call it authRoutes to be more specific and avoid any confusion.
+
 const app = express();
 
-app.get("/", (req,res)=>{
-    res.send("Server is ready.")
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(8000, () => {
-    console.log("Server is running on port 8000")
+    console.log("Server is running on port 8000!");
 });
