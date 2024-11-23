fcedc809aa949f2ed1848749d67949a0c06495b6
Pratik Kuril <pratikkuril16@gmail.com>
2024-11-23 05:45:18 +0530

    Create a connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js"; //Add the extension when importing local files
 //What is the authRoutes? authRoutes is an alias to the Router defined in auth.routes.js, meaning instead of calling it router we will call it authRoutes to be more specific and avoid any confusion.
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000; //If undefined gives 5000 by default
 
 app.use("/api/auth", authRoutes);
 
-app.listen(8000, () => {
-    console.log("Server is running on port 8000!");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}!`);
+    connectMongoDB();
 });
