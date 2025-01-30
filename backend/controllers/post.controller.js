ab32c319f170f84e7adb382ad0d91889dd8c6e42
Pratik Kuril <pratikkuril16@gmail.com>
2025-01-31 03:11:32 +0530

    Add delete post controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -34,3 +34,28 @@ export const createPost = async (req, res) => {
         console.log("Error in create post: ", error.message);  
     }
 }
+
+export const deletePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post) return res.status(400).json({message: "Post not found"});
+
+        const myUserId = req.user._id.toString();
+        if(post.user.toString() !== myUserId) {
+            return res.status(401).json({message: "You are not authorized to delete this post"});   
+        }
+
+        if(post.img) {
+            const imgId = post.img.split("/").pop().split(".")[0];
+            await cloudinary.uploader.destroy(imgId);
+        }
+
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json({message: "Post delete successfully"});
+
+    } catch (error) {
+        res.status(500).json({error:error.message});
+        console.log("Error in delete post: ", error.message);  
+    }
+}
