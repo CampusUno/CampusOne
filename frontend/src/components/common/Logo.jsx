fd96d0ff094c7a857f21f65f05c4bad860cfdd79
Pratik Kuril <pratikkuril16@gmail.com>
2025-05-18 03:08:03 +0530

    Add brand & daisy colors as tailwind variables

---
+++ frontend/src/components/common/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="font-bold text-xl">
+      Campus<span className="text-primary">One</span>
+    </div>
+  );
+};
+
+export default Logo;
--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Logo from "../../components/common/Logo";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Logo />
+      HomePage
+      <button className="btn btn-primary">Sign up</button>
+    </div>
+  );
 };
 
 export default HomePage;
