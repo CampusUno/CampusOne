f8e6c706a71dcf93322315d09825b1ee51fa17ba
Pratik Kuril <pratikkuril16@gmail.com>
2025-05-18 04:14:14 +0530

    Fix: Unable to use custom colors from var names in daisyui/theme

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Logo from "../../components/common/Logo";
+import Navbar from "../../components/common/Navbar";
 
 const HomePage = () => {
   return (
     <div>
+      <Navbar />
       <Logo />
       HomePage
       <button className="btn btn-primary">Sign up</button>
